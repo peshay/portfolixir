9574d6f9f402a9e31cc81bf3b7df45c6b0da39cc
Andreas Hubert <989010+peshay@users.noreply.github.com>
2026-04-28 13:09:14 +0200

    feat(portfolios): link securities accounts to deposit accounts

--- lib/portfolixir/portfolios/securities_account.ex
+++ lib/portfolixir/portfolios/securities_account.ex
@@ -3,6 +3,7 @@ defmodule Portfolixir.Portfolios.SecuritiesAccount do
   import Ecto.Changeset
 
   alias Portfolixir.Catalog.Currency
+  alias Portfolixir.Portfolios.DepositAccount
   alias Portfolixir.Portfolios.Portfolio
 
   schema "securities_accounts" do
@@ -11,6 +12,7 @@ defmodule Portfolixir.Portfolios.SecuritiesAccount do
     field(:notes, :string)
 
     belongs_to(:portfolio, Portfolio)
+    belongs_to(:reference_deposit_account, DepositAccount)
 
     belongs_to(:currency, Currency,
       foreign_key: :currency_code,
@@ -25,9 +27,20 @@ defmodule Portfolixir.Portfolios.SecuritiesAccount do
   @doc false
   def changeset(securities_account, attrs) do
     securities_account
-    |> cast(attrs, [:portfolio_id, :name, :currency_code, :active, :notes])
+    |> cast(attrs, [
+      :portfolio_id,
+      :reference_deposit_account_id,
+      :name,
+      :currency_code,
+      :active,
+      :notes
+    ])
     |> validate_required([:portfolio_id, :name, :currency_code])
     |> assoc_constraint(:portfolio)
+    |> foreign_key_constraint(:reference_deposit_account_id)
+    |> foreign_key_constraint(:reference_deposit_account_id,
+      name: :securities_accounts_reference_deposit_account_portfolio_fkey
+    )
     |> assoc_constraint(:currency)
   end
 end
