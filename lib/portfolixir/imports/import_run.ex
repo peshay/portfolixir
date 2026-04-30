94a278c52a7c9ef36865e7d5077fbe2953821481
Andreas Hubert <andreas.hubert@ahu.services>
2026-04-30 21:06:20 +0200

    feat: add import domain foundation

---
+++ lib/portfolixir/imports/import_run.ex
@@ -0,0 +1,26 @@
+defmodule Portfolixir.Imports.ImportRun do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Portfolixir.Imports.{ImportSource, RawImportItem}
+
+  schema "import_runs" do
+    field(:status, :string, default: "pending")
+    field(:started_at, :utc_datetime_usec)
+    field(:finished_at, :utc_datetime_usec)
+    field(:summary, :map, default: %{})
+
+    belongs_to(:import_source, ImportSource)
+    has_many(:raw_import_items, RawImportItem)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(import_run, attrs) do
+    import_run
+    |> cast(attrs, [:import_source_id, :status, :started_at, :finished_at, :summary])
+    |> validate_required([:import_source_id])
+    |> assoc_constraint(:import_source)
+  end
+end
